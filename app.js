939fe86ed891d86892a2b18eedfd9a946991be61
yinzhuo <2572703497@qq.com>
2018-07-22 17:22:38 +0800

    123

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-//app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
+  onLaunch: function () {// 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
